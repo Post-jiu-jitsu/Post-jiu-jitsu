db9e0e5c3374814d8bb14cc703b7dc0f1e394699
choyoungwoo <choyoungwoo@naver.com>
2022-08-27 23:22:47 +0900

    DB 초기설정 추가

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 //npm install --save express
 //npm run start
 //npm install cors --save
+//npm install --save mysql
 
 //터미널로 run
 //node server.js
@@ -10,6 +11,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const index = require('./routes/index');
+const db = require('./env/db');
+const { createConnection } = require('net');
 
 const PORT = 3000;
 
@@ -23,6 +26,25 @@ app.delete('/', (req, res) => res.send('delete'));
 app.post('/', (req, res) => res.send('post'));
 app.get('/', (req, res) => res.send('get'));
 app.put('/', (req, res) => res.send('put'));
+
+//DB사용법 숙지
+const dbopen = (req, res) =>{
+    db.connect();
+    console.log("dbopen");
+    db.query('SELECT * FROM users', function(error, results, fields){
+        if(error){
+            console.log(error);
+        }
+        console.log(results);
+    })
+    db.end;
+    console.log("dbclose");
+}
+
+app.get('/dbopen', dbopen);
+
+
+
 
 app.listen(PORT, () => {
     console.log('My REST API running on port ' + PORT + '!');
